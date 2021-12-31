0cad700836eba88d17d7049bb7640245701f4298
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-12-30 20:17:57 -0400

    udpate division

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ var sum = require('./sum');
 
 var multiply = require('./multiply');
 
+var division = require('./division');
+
 console.log(sum(1,5));
 console.log(multiply(4,2));
+console.log(division(4,2));
---
+++ division.test.js
@@ -0,0 +1,5 @@
+const division = require('./division');
+
+test('multiplies 2 / 4 to equal 1', () => {
+  expect(division(2, 4)).toBe(1);
+});
